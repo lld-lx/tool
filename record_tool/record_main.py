0992d5cbf6829598a03f0a2623889f9d7367afaf
lixiao <lx@xkool.org>
2021-11-08 13:54:12 +0800

    避免数据库反复连接

--- record_tool/record_main.py
+++ record_tool/record_main.py
@@ -1,5 +1,7 @@
-"""监听队列中的消息，将消息存入数据库中, 与监听部分独立(非同线程)"""
-from ctypes import CDLL, POINTER, pointer, byref
+"""监听队列中的消息，将消息存入数据库中, 与监听部分独立(非同线程)
+   保留部分疑问,双线程应该有可能导致小范围的鼠标、键盘顺序出错，但是
+   考虑到实际同时切换，严格要求顺序正确的场景很少，所以有问题了再考虑优化"""
+from ctypes import CDLL, POINTER
 from queue import Queue
 from record_tool.message import INPUT
 Que = Queue()
@@ -9,6 +11,7 @@ def listen_center():
     num = 0
     insert = CDLL("../dlls/set_sqlite.dll", winmode=0)
     struct = (POINTER(INPUT) * 10)()
+    insert.create_database(bytes("../sqlite/MKEvent.db", encoding='utf-8'))  # 创建连接，db获取句柄
     while True:
         st = Que.get()
         if st:
